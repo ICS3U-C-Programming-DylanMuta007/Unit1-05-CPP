2b869f7abded32c0bda746b8858629a7c875848e
Dylan Mutabazi <dylan.mutabazi@stu.ocsb.ca>
2025-02-19 23:47:25 +0000

    Added

---
+++ areaPerRect.cpp
@@ -0,0 +1,16 @@
+// Copyright (c) 2025 Dylan Mutabazi All rights reserved
+//
+// Created by Dylan Mutabazi
+// Created Feb 2025
+// This program calculates the area and perimeter of a rectangle
+
+#include <iostream>
+
+int main() {
+    std::cout << "If a rectangle has the dimensions:" << std::endl;
+    std::cout << "7cm X 4 cm" << std::endl;
+    std::cout << "If a rectangle has the dimensions:" << std::endl;
+    std::cout << "" << std::endl;
+    std::cout << "Area is: "<< (7*4) <<"Cm^2"<< std::endl;
+    std::cout << "Perimeter is: "<< (2*(7+4)) <<"Cm" << std::endl;
+}
